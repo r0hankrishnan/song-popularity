06ad1d54949d8d8e041d5dcf1b876dbc48a3c51f
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-06-12 00:38:13 -0400

    Update app.R

--- song-popularity-dashboard/app.R
+++ song-popularity-dashboard/app.R
@@ -1,112 +1,121 @@
-#install.packages("shinydashboard")
-library(shiny)
-library(data.table)
-library(randomForest)
 library(shinydashboard)
+library(tidyverse)
+library(randomForest)
 
-#Read in RF model
-setwd("~/Documents/GitHub/song-popularity")
-model <- readRDS("~/Documents/GitHub/song-popularity/models/rfModel.rds")
-explicitChoice <- c(TRUE, FALSE)
-keyChoice <- c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
-timesigChoice <- c(1, 3, 4, 5)
-genreChoice <- c("jazz", "pop", "rock")
-varnames <- colnames(train)[-1]
+songs <- read.csv("~/Documents/GitHub/song-popularity/data/clean_data.csv")
+songs <- songs %>% select(-id)
+range(songs$danceability)
+keyChoice <- sort(as.vector(unique(songs$key)))
+timeChoice <- sort(as.vector(unique(songs$time_signature)))
+genreChoice <- as.vector(unique(songs$track_genre))
 
-ui <- pageWithSidebar(
-  #Page Header
-  headerPanel("Song Popularity Predictor"),
-  
-  #Input values
-  sidebarPanel(
-    tags$label(h3("Input Data")),
-    numericInput("duration_ms", label = "Song duration", value = 0.0),
-    # radioButtons("explicit", label = "Is the song explicit?", 
-    #              choices = explicitChoice, selected = character(0),
-    #              inline = FALSE),
-    # numericInput("danceability", label = "Song danceability", value = 0.0),
-    # numericInput("energy", label = "Song energy", value = 0.0),
-    # selectInput("key", label = "Song key", choices = keyChoice,
-    #              selected = NULL),
-    # numericInput("loudness", label = "Song loudness", value = 0.0),
-    # numericInput("mode", label = "Song mode", value = 0.0),
-    # numericInput("speechiness", label = "Song speechiness", value = 0.0),
-    # numericInput("acousticness", label = "Song acousticness", value = 0.0),
-    # numericInput("instrumentalness", label = "Song instrumentalness", 
-    #              value = 0.0),
-    # numericInput("liveness", label = "Song liveness", value = 0.0),
-    # numericInput("valence", label = "Song valence", value = 0.0),
-    # numericInput("tempo", label = "Song tempo", value = 0.0),
-    # selectInput("time_signature", label = "Song time signature", 
-    #             choices = timesigChoice, selected = NULL),
-    # selectInput("track_genre", label = "Song genre",
-    #             choices = genreChoice, selected = NULL),
+
+ui <- dashboardPage(
+  skin = "black",
+
+  dashboardHeader(title = "Song Popularity Prediction", titleWidth = 250),
+  dashboardSidebar(
+    tags$head(tags$style(".wrapper {overflow: visible !important;}")),
+    #Input variables
+    numericInput(inputId = "duration_ms", label = "Song Duration (ms)",
+                min = 0, max = 1000000, value = 0),
     
-    #Submit values
-    actionButton("submitbutton", "Submit", class = "btn btn-primary")
+    checkboxInput(inputId = "explicit", label = "Is the Song Explicit?",
+                  value = FALSE),
+    
+    numericInput(inputId = "danceability", 
+                 label = "Song Danceability Score (0 to 1)",
+                min = 0, max = 1.00, value = 0),
+    
+    numericInput(inputId = "energy", label = "Song Energy Score (0 to 1)",
+                 min = 0, max = 1.00, value = 0),
+    
+    selectInput(inputId = "key", label = "What is the Song's Key?",
+                choices = keyChoice, selected = NULL, width = "100%"),
+    
+    numericInput(inputId = "loudness", label = "Song Loudness (0 to -100)", 
+                 min = -100, max = 0, value = 0),
+    
+    selectInput(inputId = "mode", label = "Song Mode", 
+                choices = c(0,1), selected = 0, width = "100%"),
+    
+    numericInput(inputId = "speechiness", label = "Song Speechiness (0 to 1)",
+                 min = 0, max = 1, value = 0),
+    
+    numericInput(inputId = "acousticness", label = "Song Acousticness (0 to 1)",
+                 min = 0, max = 1, value = 0),
+    
+    numericInput(inputId = "instrumentalness", 
+                 label = "Song Instrumentalness (0 to 1)",
+                 min = 0, max = 1, value = 0),
+    
+    numericInput(inputId = "liveness", 
+                 label = "Song Liveness (0 to 1)",
+                 min = 0, max = 1, value = 0),
+    
+    numericInput(inputId = "valence", 
+                 label = "Song Valence (0 to 1)",
+                 min = 0, max = 1, value = 0),
+    
+    numericInput(inputId = "tempo", 
+                 label = "Song Tempo",
+                 min = 0, max = 300, value = 0),
+    
+    selectInput(inputId = "time_signature",
+                label = "Song Time Signature",
+                choices = timeChoice, selected = NULL, width = "100%"),
+    
+    selectInput(inputId = "track_genre",
+                label = "What is the Song's Genre?",
+                choice = genreChoice, selected = NULL, width = "100%"),
+    
+    actionButton(inputId = "action",
+                 label = "Run Popularity Prediction")
   ),
   
-  mainPanel(
-    tags$label(h3("Status/Output")),
-    verbatimTextOutput("contents"),
-    tableOutput("tabledata")
+  dashboardBody(
+    box(textOutput("text_result"))
+   
+  
   )
-)
+    )
 
-
-
-server <- function(input, output, session) { 
-  print(input$duration_ms)
-  #Input data
-  # datasetInput <- reactive({
-  #   
-  #   df <- data.frame(
-  #     Name = varnames,
-  #     Value = as.character(c(input$duration_ms,
-  #               input$explicit,
-  #               input$danceability,
-  #               input$energy,
-  #               input$key,
-  #               input$loudness,
-  #               input$mode,
-  #               input$speechiness,
-  #               input$acousticness,
-  #               input$instrumentalness,
-  #               input$liveness,
-  #               input$valence,
-  #               input$tempo,
-  #               input$time_signature,
-  #               input$track_genre)),
-  #     stringsAsFactors = FALSE
-  #   )
-  #   
-  #   #popularity <- 0
-  #   #df <- rbind(df, popularity)
-  #   input <- transpose(df)
-  #   write.table(input, "input.csv", sep = ",", quote = FALSE, 
-  #               row.names = FALSE, col.names = FALSE)
-  #   
-  #   test <- read.csv(paste("input", ".csv", sep = ""), header = TRUE)
-  #   output <- data.frame(prediction = predict(model, test), 
-  #                        round(predict(model, test, type = "prob"), 3))
-  #   print(output)
-  # })
-  # 
-  # #Status/Output text box
-  # output$contents <- renderPrint({
-  #   if(input$submitbutton > 0){
-  #     isolate("Calculation complete.")
-  #   } else{
-  #     return("Server is ready for calculation.")
-  #   }
-  # })
-  # 
-  # #Prediction results table
-  # output$tabledata <- renderTable({
-  #   if(input$submitbutton > 0){
-  #     datasetInput()
-  #  }
-  #})
-  }
+server <- function(input, output) {
+  testData <- eventReactive(input$action, {
+    newData <- data.frame(
+      duration_ms = input$duration_ms,
+      explicit = input$explicit,
+      danceability = input$danceability,
+      energy = input$energy,
+      key = as.numeric(input$key),
+      loudness = input$loudness,
+      mode = as.numeric(input$mode),
+      speechiness = input$speechiness,
+      acousticness = input$acousticness,
+      instrumentalness = input$instrumentalness,
+      liveness = input$liveness,
+      valence = input$valence,
+      tempo = input$tempo,
+      time_signature = as.numeric(input$time_signature),
+      track_genre = input$track_genre
+    )
+  })
+  
+  prediction <- eventReactive(input$action, {
+    rf_model <- readRDS("~/Documents/GitHub/song-popularity/song-popularity-dashboard/randomforest.RDS")
+    
+    
+    pred <- predict(rf_model, testData(), type = "response")
+    
+    pred
+  })
+  
+  output$text_result <- renderText({
+    predNum <- as.numeric(prediction())
+    
+    paste0("The song's predicted popularity is: ", round(predNum,2))
+  })
+  
+}
 
 shinyApp(ui, server)
